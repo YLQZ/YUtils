f33adc8dbf6e3086df98c8d04f5a89befa48937c
1194979849@qq.com <1194979849@qq.com>
2020-05-19 22:13:14 +0800

    up  dist

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.YUtils = void 0;
+var YUtils = /** @class */ (function () {
+    function YUtils() {
+    }
+    return YUtils;
+}());
+exports.YUtils = YUtils;
